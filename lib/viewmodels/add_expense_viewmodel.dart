f3e535e9d757bdad65b1a229b9994dbf405d1af5
Umut Aktepe <umutaktepe98@gmail.com>
2026-01-06 23:56:54 +0300

    Fancy radio buttons

--- lib/viewmodels/add_expense_viewmodel.dart
+++ lib/viewmodels/add_expense_viewmodel.dart
@@ -6,9 +6,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import '../models/transaction_model.dart';
 
+enum SplitOption {
+  youPaidSplit,
+  youPaidFull,
+  partnerPaidSplit,
+  partnerPaidFull,
+}
+
 class AddExpenseViewModel extends ChangeNotifier {
   final AddExpenseScreenState _state = AddExpenseScreenState();
   AddExpenseScreenState get state => _state;
+
+  SplitOption _selectedOption = SplitOption.youPaidSplit;
+  SplitOption get selectedOption => _selectedOption;
+
+  String _partnerName = 'Partner';
+  String get partnerName => _partnerName;
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -20,6 +33,39 @@ class AddExpenseViewModel extends ChangeNotifier {
   void setLoading(bool loading) {
     _isLoading = loading;
     notifyListeners();
+  }
+
+  Future<void> init(String partnerUid) async {
+    try {
+      final doc = await _firestore.collection('users').doc(partnerUid).get();
+      if (doc.exists) {
+        final data = doc.data();
+        _partnerName = data?['displayName'] ?? 'Partner';
+        notifyListeners();
+      }
+    } catch (e) {
+      debugPrint("Error fetching partner name: $e");
+    }
+  }
+
+  void setSplitOption(SplitOption option) {
+    _selectedOption = option;
+    notifyListeners();
+  }
+
+  String getDescriptionText(double amount) {
+    if (amount <= 0) return '';
+
+    switch (_selectedOption) {
+      case SplitOption.youPaidSplit:
+        return '$_partnerName owes you ${(amount / 2).toStringAsFixed(2)} ₺';
+      case SplitOption.youPaidFull:
+        return '$_partnerName owes you ${amount.toStringAsFixed(2)} ₺';
+      case SplitOption.partnerPaidSplit:
+        return 'You owe $_partnerName ${(amount / 2).toStringAsFixed(2)} ₺';
+      case SplitOption.partnerPaidFull:
+        return 'You owe $_partnerName ${amount.toStringAsFixed(2)} ₺';
+    }
   }
 
   Future<void> pickImage(ImageSource source) async {
@@ -62,11 +108,40 @@ class AddExpenseViewModel extends ChangeNotifier {
       }
 
       // 2. Create Transaction Object
+      // 2. Create Transaction Object
+      double finalAmount = amount;
+      String finalSender = _auth.currentUser!.uid;
+      String finalReceiver = receiverUid;
+
+      // Logic adjustments based on selection
+      switch (_selectedOption) {
+        case SplitOption.youPaidSplit:
+          finalAmount = amount / 2;
+          finalSender = _auth.currentUser!.uid;
+          finalReceiver = receiverUid;
+          break;
+        case SplitOption.youPaidFull:
+          finalAmount = amount;
+          finalSender = _auth.currentUser!.uid;
+          finalReceiver = receiverUid;
+          break;
+        case SplitOption.partnerPaidSplit:
+          finalAmount = amount / 2;
+          finalSender = receiverUid;
+          finalReceiver = _auth.currentUser!.uid;
+          break;
+        case SplitOption.partnerPaidFull:
+          finalAmount = amount;
+          finalSender = receiverUid;
+          finalReceiver = _auth.currentUser!.uid;
+          break;
+      }
+
       final newTx = TransactionModel(
         id: '', // Firestore generates this
-        senderUid: _auth.currentUser!.uid,
-        receiverUid: receiverUid,
-        amount: amount,
+        senderUid: finalSender,
+        receiverUid: finalReceiver,
+        amount: finalAmount,
         note: note,
         photoUrl: photoUrl,
         timestamp: DateTime.now(),
